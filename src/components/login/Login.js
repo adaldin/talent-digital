e144f6afabfeb9c61442ac774069776a35069ad4
Hiena <aledaldin@gmail.com>
2022-10-03 10:46:26 +0200

    Logon-register with firebase

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,30 +1,70 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
+import Alert from "react-bootstrap/Alert";
+import { useAuth } from "../../context/AuthContext";
 
 function Login() {
-  return (
-    <Row as={Form} className="p-3">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState("");
+  const { login } = useAuth();
+  const navigate = useNavigate();
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <div className="d-flex justify-content-center">
-        <Link to={"/register"}>New user? Create an Account.</Link>
-      </div>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Row>
+  // logic
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoginError("");
+    try {
+      await login(user.email, user.password);
+      navigate("/dropper");
+    } catch (error) {
+      console.log(error.message);
+      setLoginError("Correo inválido");
+    }
+  };
+
+  return (
+    <>
+      {loginError && <Alert variant="danger">{loginError}</Alert>}
+      <Row as={Form} className="p-3" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <div className="d-flex justify-content-center">
+          <Link to={"/register"}>New user? Create an Account.</Link>
+        </div>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Row>
+    </>
   );
 }
 
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,40 +1,93 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { Link } from "react-router-dom";
+import { Alert } from "react-bootstrap";
+import { useAuth } from "../../context/AuthContext";
 
 function Register() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    fName: "",
+    lName: "",
+  });
+  const [registerError, setRegisterError] = useState("");
+  const { signUp } = useAuth();
+  const navigate = useNavigate();
+
+  //   logic
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setRegisterError("");
+    try {
+      await signUp(user.email, user.password, user.fName, user.lName);
+      navigate("/login");
+    } catch (error) {
+      if (
+        error.code === "auth/email-already-in-use" ||
+        error.code === "auth/internal-error"
+      )
+        setRegisterError("Correo inválido");
+    }
+  };
+
   return (
-    <Row as={Form} className="p-3">
-      <Form.Group className="mb-3" controlId="formBasicFirstName">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter your first name" />
-      </Form.Group>
+    <>
+      {registerError && <Alert variant="danger">{registerError}</Alert>}
+      <Row as={Form} className="p-3" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicFirstName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your first name"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicLastName">
-        <Form.Label>Last name</Form.Label>
-        <Form.Control type="text" placeholder="Enter your last name" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicLastName">
+          <Form.Label>Last name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your last name"
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <div className="d-flex justify-content-center">
-        <Link to={"/login"}>Old user? Login.</Link>
-      </div>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Row>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <div className="d-flex justify-content-center">
+          <Link to={"/login"}>Old user? Login.</Link>
+        </div>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Row>
+    </>
   );
 }
 export default Register;
