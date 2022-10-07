9c7597a7b3ecde1313e6233c76b14af89f775697
Hiena <aledaldin@gmail.com>
2022-10-07 16:00:50 +0200

    updated to deploy

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { keys } from "../config";
+import { keys } from "../config.js";
 
 const firebaseConfig = keys;
 
